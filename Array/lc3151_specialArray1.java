c888f6096799ecf9828039966996f3019cbf44dc
Manish <www.mehta70@gmail.com>
2026-07-20 20:45:29 +0530

    Solved lc 3151

---
+++ Array/lc3151_specialArray1.java
@@ -0,0 +1,11 @@
+public class lc3151_specialArray1 {
+    public boolean isArraySpecial(int[] nums) {
+        if (nums.length == 1) return true;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if ((nums[i] % 2 == 0) && (nums[i + 1] % 2 == 0) || ((nums[i] % 2 != 0) && nums[i + 1] % 2 != 0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
