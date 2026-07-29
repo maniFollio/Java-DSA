0f544b90e45d3a6010dc263585662459b60c7318
Manish <www.mehta70@gmail.com>
2026-07-29 20:54:39 +0530

    Solved lc 142

---
+++ LinkedList/lc142_linkedListDetectionII.java
@@ -0,0 +1,34 @@
+public class lc142_linkedListDetectionII {
+    ListNode head;
+
+    class ListNode {
+        int data;
+        ListNode next;
+
+        ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
--- LinkedList/lc19_removeNthNode.java
+++ LinkedList/lc19_removeNthNode.java
@@ -3,33 +3,9 @@ public class lc19_removeNthNode {
     class ListNode {
         int data;
         ListNode next;
-
         ListNode(int data) {
             this.data = data;
             this.next = null;
         }
-    }
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode fast = head;
-        ListNode slow = head;
-
-        if (head.next == null) {
-            return null;
-        }
-
-        for (int i = 0; i < n; i++) {
-            fast = fast.next;
-        }
-
-        if (fast == null) {
-            return head.next;
-        }
-
-        while (fast.next != null) {
-            slow = slow.next;
-            fast = fast.next;
-        }
-        slow.next = slow.next.next;
-        return head;
     }
 }
