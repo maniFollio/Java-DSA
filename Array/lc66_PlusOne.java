440665061df3caada7e073555afe4f9964376421
Manish <www.mehta70@gmail.com>
2026-08-08 16:11:59 +0530

    lc 66

--- Array/lc66_PlusOne.java
+++ Array/lc66_PlusOne.java
@@ -2,25 +2,22 @@ import java.util.Arrays;
 
 public class lc66_PlusOne {
     int[] plusOne(int[] nums) {
-        int carry = 0;
-        int digit = 0;
-        int idx = nums.length + 1;
-        int[] ans = new int[idx];
-        nums[nums.length - 1] += 1;
-        for (int i = nums.length - 1; i >= 0;) {
-            int n = nums[i];
-            if (nums[i] == 10) {
-                carry = n / 10;
-                digit = n % 10;
-                ans[idx--] = digit;
-                nums[i--] += carry;
-                }
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if(nums[i] == 9){
+                nums[i] = 0;
+            } else {
+                nums[i] += 1;
+                return nums;
+            }
         }
-        return ans;
+        nums = new int[nums.length + 1];
+        nums[0] = 1;
+        return nums;
     }
 
     public static void main(String[] args) {
         // int[] arr = { 9, 9, 9 };
+        // int[] arr = { 1, 2, 9 };
         // int[] arr = { 1, 2, 3 };
         int[] arr = { 9 };
         lc66_PlusOne lc66 = new lc66_PlusOne();
