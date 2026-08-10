70cf1f3685977b3057f7680d7d1a90f477964f20
Manish <www.mehta70@gmail.com>
2026-08-10 21:54:03 +0530

    lc 219 Contains Duplicate II

---
+++ Array/lc219_containsDuplicateII.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class lc219_containsDuplicateII {
+    boolean containsDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int prevIdx = map.get(nums[i]);
+                if( i-prevIdx <=k){
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 1 };
+        int k = 3;
+        lc219_containsDuplicateII lc219 = new lc219_containsDuplicateII();
+        boolean res = lc219.containsDuplicate(arr, k);
+        System.out.println(res);
+    }
+}
