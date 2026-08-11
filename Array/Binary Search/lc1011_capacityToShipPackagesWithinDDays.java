20747ab5cac4fe212b274a5e11f51f33deb37f4f
Manish <www.mehta70@gmail.com>
2026-08-11 22:36:39 +0530

    lc 1011 Capacity to Ship Package in D Days

---
+++ Array/Binary Search/lc1011_capacityToShipPackagesWithinDDays.java
@@ -0,0 +1,45 @@
+public class lc1011_capacityToShipPackagesWithinDDays {
+    int capacityTOShip(int[] weights, int days) {
+        int left = 0;
+        int right = 0;
+        for (int weight : weights) {
+            left = Math.max(left, weight);
+            right += weight;
+        }
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isCapacityEnough(weights, days, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        // System.out.println(left);
+        return left;
+    }
+
+    boolean isCapacityEnough(int[] weights, int days, int capacity) {
+        int currentWeight = 0;
+        int day = 1;
+        for (int weight : weights) {
+            if (currentWeight + weight > capacity) {
+                day++;
+                currentWeight = weight;
+            } else {
+                currentWeight += weight;
+            }
+        }
+        return day <= days;
+    }
+
+    public static void main(String[] args) {
+        lc1011_capacityToShipPackagesWithinDDays lc1011 = new lc1011_capacityToShipPackagesWithinDDays();
+        // int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        // int days = 5;
+        int[] arr = { 3,2,2,4,1,4};
+        int days = 3;
+        int res = lc1011.capacityTOShip(arr, days);
+        System.out.println(res);
+    }
+
+}
--- Array/lc219_containsDuplicateII.java
+++ Array/lc219_containsDuplicateII.java
@@ -3,10 +3,10 @@ import java.util.HashMap;
 public class lc219_containsDuplicateII {
     boolean containsDuplicate(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(nums[i])){
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
                 int prevIdx = map.get(nums[i]);
-                if( i-prevIdx <=k){
+                if (i - prevIdx <= k) {
                     return true;
                 }
             }
@@ -14,7 +14,6 @@ public class lc219_containsDuplicateII {
         }
         return false;
     }
-
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 1 };
         int k = 3;
