6321c83fb7bca7705a4e20e5469fb2405b8c7ffe
Manish <www.mehta70@gmail.com>
2026-08-12 21:55:11 +0530

    lc 1470

--- Array/lc1470_shuffletheArray.java
+++ Array/lc1470_shuffletheArray.java
@@ -1,16 +1,15 @@
 import java.util.Arrays;
 
 public class lc1470_shuffletheArray {
-    int[] shuffleTheArray(int[] nums, int n) {
+    int[] shuffleTheArray(int[] nums, int n) { // n is the half size of the array.
         int[] og = new int[nums.length];
-        int len = (nums.length / 2); // n can also be used inplace of len.
         int idx = 0;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < n; i++) {
             og[idx] = nums[i];
             idx += 2;
         }
         idx = 1;
-        for (int i = len; i < nums.length; i++) {
+        for (int i = n; i < nums.length; i++) {
             og[idx] = nums[i];
             idx += 2;
         }
@@ -21,8 +20,9 @@ public class lc1470_shuffletheArray {
         // int[] arr = { 1, 2, 3, 4, 4, 3, 2, 1 };
         // int[] arr = { 1, 1, 2, 2 };
         int[] arr = { 2, 5, 1, 3, 4, 7 };
+        int n = 3;
         lc1470_shuffletheArray lc1470 = new lc1470_shuffletheArray();
-        int[] res = lc1470.shuffleTheArray(arr, 4);
+        int[] res = lc1470.shuffleTheArray(arr,n);
         System.out.println(Arrays.toString(res));
     }
 }
