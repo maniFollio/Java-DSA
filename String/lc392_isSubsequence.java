a423d2a86c371a5b4155b1ff5fd78537f2accbe5
Manish <www.mehta70@gmail.com>
2026-08-15 22:14:25 +0530

    lc 392 Is Subsequence

---
+++ String/lc392_isSubsequence.java
@@ -0,0 +1,21 @@
+public class lc392_isSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        String t = "ahbgdc";
+        lc392_isSubsequence lc392 = new lc392_isSubsequence();
+        boolean res = lc392.isSubsequence(s, t);
+        System.out.println(res);
+    }
+}
