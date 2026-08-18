b2df69efd37d198d31833c27c20a948abf1c4d93
Manish <www.mehta70@gmail.com>
2026-08-18 22:01:40 +0530

    lc 448 find All Numbers Disappeared in an Array

--- Array/lc448_findAllNumbersDisappearedinanArray.java
+++ Array/lc448_findAllNumbersDisappearedinanArray.java
@@ -3,20 +3,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public class lc448_findAllNumbersDisappearedinanArray {
-    public void findDisappearedNumbers(int[] nums) {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
         ArrayList<Integer> list = new ArrayList<>();
         Arrays.sort(nums);
-        int i ;
-        for (i = 0; i < nums.length; i++) {
-            if(nums[i] != i+1){
-                list.add(i);
+        int[] arr = new int[nums.length];
+        int idx = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                arr[idx++] = nums[i - 1];
             }
         }
+        arr[idx++] = nums[nums.length - 1];
+        int count = 1;
+        int i = 0;
+        while (count <= arr.length) {
+            if (i < idx && arr[i] == count) {
+                i++;
+            } else {
+                list.add(count);
+            }
+            count++;
+        }
         System.out.println(list);
+        return list;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 4, 3, 2, 7, 8, 2, 3, 1 };
+        // int[] arr = { 4, 3, 2, 7, 8, 2, 3, 1 };
+        int[] arr = { 1, 1 };
         lc448_findAllNumbersDisappearedinanArray lc448 = new lc448_findAllNumbersDisappearedinanArray();
         lc448.findDisappearedNumbers(arr);
     }
