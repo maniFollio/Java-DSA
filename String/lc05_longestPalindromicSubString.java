ae499862499d728d4f6461cff54889fad20fc6f5
Manish <www.mehta70@gmail.com>
2026-08-19 23:10:53 +0530

    lc 05 longest palindrome substring

---
+++ String/lc05_longestPalindromicSubString.java
@@ -0,0 +1,45 @@
+public class lc05_longestPalindromicSubString {
+
+    String longestPalindrome(String str) {
+        if (str.length() <= 1) {
+            return str;
+        }
+        String LPS = "";
+        for (int i = 1; i < str.length(); i++) {
+            // Consider odd length
+            int low = i;
+            int high = i;
+            while (str.charAt(low) == str.charAt(high)) {
+                low--;
+                high++;
+                if (low == -1 || high == str.length())
+                    break;
+            }
+            String palindrome = str.substring(low + 1, high);
+            if (palindrome.length() > LPS.length()) {
+                LPS = palindrome;
+            }
+            // Consider even length
+            low = i - 1;
+            high = i;
+            while (str.charAt(low) == str.charAt(high)) {
+                low--;
+                high++;
+                if (low == -1 || high == str.length())
+                    break;
+            }
+            palindrome = str.substring(low + 1, high);
+            if (palindrome.length() > LPS.length()) {
+                LPS = palindrome;
+            }
+        }
+        return LPS;
+    }
+
+    public static void main(String[] args) {
+        String s = "babad";
+        lc05_longestPalindromicSubString lc05 = new lc05_longestPalindromicSubString();
+        System.out.println(lc05.longestPalindrome(s));
+    }
+
+}
