7baeda8c9196cfd1c3cf3905755deefb2aac6dc7
Manish <www.mehta70@gmail.com>
2026-09-06 22:34:50 +0530

    lc 326. Power Of 4

---
+++ Maths/lc342_powerOfFour.java
@@ -0,0 +1,11 @@
+public class lc342_powerOfFour {
+    public boolean isPowerOfFour(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        while (num % 4 == 0) {
+            num = num / 4;
+        }
+        return num == 1;
+    }
+}
