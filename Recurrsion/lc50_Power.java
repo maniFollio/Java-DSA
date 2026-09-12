6ec6d08c20693617213fca7d5b15f8ebe70436ee
Manish <www.mehta70@gmail.com>
2026-09-12 23:13:15 +0530

    Power(x, n)

--- Recurrsion/lc50_Power.java
+++ Recurrsion/lc50_Power.java
@@ -1,14 +1,23 @@
 public class lc50_Power {
     public double myPow(double x, int n) {
-        if (n == 0) {
+        long power = n;
+        if (n < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+        if (power== 0) {
             return 1.0;
         }
-        return myPow(x, n - 1) * x;
+        if (power% 2 == 0) {
+            return myPow(x * x, (int)(power/ 2));
+        } else {
+            return myPow(x * x, (int)(power/ 2)) * x;
+        }
     }
 
     public static void main(String[] args) {
         double x = 2.00000;
-        int n = 10;
+        int n = -2;
         lc50_Power lc50 = new lc50_Power();
         System.out.println(lc50.myPow(x, n));
     }
